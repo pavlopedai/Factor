e2d76b28583baaef222704962683389070c7f29d
Pavlo Pedai <pavlopedai@gmail.com>
2019-12-10 13:02:09 +0200

    Initial Commit

---
+++ Factor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Factor
+//
+//  Created by Pavlo Pedai on 10/12/19.
+//  Copyright © 2019 Pavlo Pedai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
